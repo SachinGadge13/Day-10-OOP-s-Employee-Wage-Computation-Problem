223e834d8cbe7536f0fc274a812e2c8c229c5d8d
SachinGadge13 <gadges14@gmail.com>
2022-10-12 19:02:55 +0530

    Added:Program to store Daily wage along with the Total Wage

--- src/com/bridgelabz/employeewage/Company.java
+++ src/com/bridgelabz/employeewage/Company.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Arrays;
+
 public class Company {
 	public final String companyName;
 	public final int empRatePerHr;
 	public final int numberOfWorkingDays;
 	public final int maxWorkingHrsPerMonth;
+	public int totalEmpWage = 0;
+	public int[] dailyWage = new int[20];
 
 	public Company(String companyName, int empRatePerHr, int numberOfWorkingDays, int maxWorkingHrsPerMonth) {
 		super();
@@ -12,6 +16,11 @@ public class Company {
 		this.empRatePerHr = empRatePerHr;
 		this.numberOfWorkingDays = numberOfWorkingDays;
 		this.maxWorkingHrsPerMonth = maxWorkingHrsPerMonth;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [totalEmpWage=" + totalEmpWage + "]\n" + "dailyWage=" + Arrays.toString(dailyWage);
 	}
 
 }
--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,16 +2,16 @@ package com.bridgelabz.employeewage;
 
 import java.util.ArrayList;
 
-public class EmpWageBuilder {
+public class EmployeeWage {
 	// constants
 	final static int IS_FULL_TIME = 1;
 	final static int IS_PART_TIME = 2;
-
 	// instance variables
 	ArrayList<Company> companyList = new ArrayList<Company>();
 
-	// To add multiple companies with their parameters to calculate employee wage
-	public void addCompanyDetailsForEmpWage(String companyName, int empRatePerHr, int numnberOfWorkingDays,
+	// To add multiple companies with their parameters to calculate employee
+	// wage
+	public void addCompanyDetailsForEmpWage( String companyName, int empRatePerHr, int numnberOfWorkingDays,
 			int maxWorkingHrsPerMonth) {
 		Company company = new Company(companyName, empRatePerHr, numnberOfWorkingDays, maxWorkingHrsPerMonth);
 		companyList.add(company);
@@ -21,6 +21,7 @@ public class EmpWageBuilder {
 	public void companyEmpWage() {
 		for (int i = 0; i < companyList.size(); i++) {
 			empWgaeComputation(this.companyList.get(i));
+			System.out.println(this.companyList.get(i).toString());
 		}
 	}
 
@@ -49,17 +50,13 @@ public class EmpWageBuilder {
 	public void empWgaeComputation(Company company) {
 		int days = 0;
 		int totalWorkingHrs = 0;
-		int totalEmpWage = 0;
-
 		while (days < company.numberOfWorkingDays && totalWorkingHrs <= company.maxWorkingHrsPerMonth) {
 			days++;
-
 			int empHrs = this.getEmpHrs();
 			totalWorkingHrs += empHrs;
-
-			System.out.println("emp hrs : " + empHrs);
+			company.dailyWage[days] = empHrs * company.empRatePerHr;
 		}
-		totalEmpWage = totalWorkingHrs * company.empRatePerHr;
-		System.out.println("Total employee wage for company " + company.companyName + " is " + totalEmpWage);
+		company.totalEmpWage = totalWorkingHrs * company.empRatePerHr;
 	}
+
 }
